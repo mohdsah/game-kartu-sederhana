648915a31cf5125246d50be42564c83c7a1d66b2
mohdsah <mohdsah5@gmail.com>
2025-04-26 23:22:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const cards = ["kartu1.png", "kartu2.png", "kartu3.png", "kartu4.png", "kartu5.png", "kartu6.png", "kartu7.png"];
-const gameBoard = document.getElementById('gameBoard');
-
-function createBoard() {
-    for (let i = 0; i < cards.length * 2; i++) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        const img = document.createElement('img');
-        img.src = 'assets/' + cards[i % cards.length];
-        card.appendChild(img);
-        gameBoard.appendChild(card);
-    }
+function markBox(element) {
+  element.classList.toggle('marked');
 }
-
-createBoard();
